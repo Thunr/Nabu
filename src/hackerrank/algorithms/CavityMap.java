c8526c0b106db41f854074d3fd0e43f684be249f
Tauri Hindre <tauri.hindre@gmail.com>
2016-01-04 09:45:47 +0200

    asd

---
+++ src/hackerrank/algorithms/CavityMap.java
@@ -0,0 +1,10 @@
+package hackerrank.algorithms;
+import java.util.Scanner;
+public class CavityMap {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
