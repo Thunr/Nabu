f41df6cd3f1a8296c76b643822c775e7afec1bf9
Tauri Hindre <tauri.hindre@gmail.com>
2016-01-22 06:36:17 +0200

    Linkedlist improvements

--- src/hackerrank/datastructures/linkedlists/LinkedListV1.java
+++ src/hackerrank/datastructures/linkedlists/LinkedListV1.java
@@ -79,6 +79,58 @@ public class LinkedListV1 {
 			return null;
 	}
 	
+	Node MergeLists(Node headA, Node headB) {
+	    if(headA==null)
+	        return headB;
+	    if(headB==null)
+	        return headA;
+	    Node firstNode;
+	    if(headA.data<headB.data){
+	        firstNode = headA;
+	        headA = headA.next;
+	    }
+	    else{
+	        firstNode = headB;
+	        headB = headB.next;
+	    }
+	    Node newHead = firstNode;
+	    while(headB!=null&&headA!=null){
+	        if(headA.data<headB.data){
+	            firstNode.next = headA;
+	            headA = headA.next;
+	        }
+	        else{
+	            firstNode.next = headB;
+	            headB = headB.next;
+	        }
+	        firstNode = firstNode.next;
+	    }
+	    if(headB==null)
+	        firstNode.next = headA;
+	    else
+	        firstNode.next = headB;
+	    return newHead;
+
+	}
+	
+	Node RemoveDuplicates(Node head1) {
+	    if(head1==null)
+	        return head1;
+	    else{
+	        Node head = head1;
+	       while(head.next!=null){
+	         if(head.data==head.next.data)
+	             head.next=head.next.next;
+	         else{
+	             head = head.next;
+	         }
+	       }
+	        return head1;
+	    }
+	    
+
+	}
+	
 	
 	
 }
